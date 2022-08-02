aa403c19d9f11d0bef37a5ee8a69fa8aa49023bd
a-programmer-guy <ctrl_kay@protonmail.com>
2022-08-02 07:34:35 -0600

    Add endpoint
    This commit adds an endpoint for the application to serve a list of
    string type upon a GET request.

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController // Annotation gives this class RESTful functionality
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	@GetMapping // Annotation allows server to serve/respond to GET requests at this endpoint
+	public List<String> hello() {
+		return List.of("Hello", "World");
+	}
+	
 }
