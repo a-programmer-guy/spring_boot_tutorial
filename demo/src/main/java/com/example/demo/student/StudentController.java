ddfabd0c8070060fa077a612cd726edd8e3c8e93
a-programmer-guy <ctrl_kay@protonmail.com>
2022-08-02 18:46:26 -0600

    Add Service layer
    This commit adds the StudentService Class which serves as the applications
    service layer to communicate with the API layer.
    
    Dependency injection is utilized to bring in the methods and data
    from the StudentService Class to the SudentController.

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -1,25 +1,29 @@
 package com.example.demo.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // gives this class RESTful functionality
-@RequestMapping(path = "api/v1/student")// Routes HTTP reqs to handler methods/controllers 
+@RequestMapping(path = "api/v1/student")// Routes HTTP requests
 public class StudentController {
+	
+	// Access methods inside StudentService with instance variable
+	private final StudentService studentService;
+	
+	// Constructor for StudentController with StudentService parameter
+	// Uses dependency injection
+	@Autowired // Autowires(instantiates) and injects StudentService into the constructor
+	 public StudentController(StudentService studentService) {
+		this.studentService = studentService;
+	}
+	
 	@GetMapping // Annotation allows server to serve/respond to GET requests
 	public List<Student> getStudents() {
-		return List.of(
-				new Student(
-						1L,
-						"Tony",
-						"Soprano@gmail.com",
-						LocalDate.of(2000,Month.APRIL,20),
-						20
-						));
+		return studentService.getStudents();
 	}
+
 }
